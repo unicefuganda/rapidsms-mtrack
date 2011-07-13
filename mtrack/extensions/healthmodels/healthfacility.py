e02d4ccd6e6801a811f83accbe3c431f360fffda
Rowena <rluk@dimagi.com>
2011-07-13 12:37:38 +0300

    change FacilityWithDrugs to have a foreignkey to supply point (instead of extending it)

--- mtrack/extensions/healthmodels/healthfacility.py
+++ mtrack/extensions/healthmodels/healthfacility.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
+from django.db import models
 from logistics.models import SupplyPoint
 
-class FacilityWithDrugs(SupplyPoint):
+class FacilityWithDrugs(models.Model):
+    supply_point = models.ForeignKey("logistics.SupplyPoint", null=True, blank=True)
+    
     class Meta:
         abstract = True
