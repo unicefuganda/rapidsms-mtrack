9a54b43511f8687e2700a080c809247a40ab31e0
Rowena <rluk@dimagi.com>
2011-07-13 12:38:09 +0300

    refactor db test loader utils so that they can be used inside of system init's or test scripts more easily

---
+++ mtrack/loader.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from django.conf import settings
+from rapidsms.contrib.locations.models import LocationType, Location, Point
+from dimagi.utils.couch.database import get_db
+from logistics.const import Reports
+from logistics.models import SupplyPoint, SupplyPointType,\
+    ProductReportType, ContactRole, Product, ProductType
+from logistics.util import config
+
+def load_cvs_xforms():
+    from cvs.utils import init_xforms
+    init_xforms()
+    
+def  _init_facility_types():
+    from healthmodels.models import HealthFacilityType
+    from logistics.util import config
+    for code, name in config.SupplyPointTypes.ALL.items():
+        type_ = HealthFacilityType.objects.get_or_create(slug=code)[0]
+        if type_.name != name:
+            type_.name = name
+            type_.save()
+
+def init_test_facilities(log_to_console=False):
+    # this assumes all supply point codes will be the same as facility codes
+    # and that all supply points are clinics
+    from healthmodels.models import HealthFacility, HealthFacilityType
+    from logistics.models import SupplyPoint, SupplyPointType
+    _init_facility_types()
+    loc = Location.objects.all()[0]
+    sp_type = SupplyPointType.objects.get(code=config.SupplyPointTypes.CLINIC)
+    sp, created = SupplyPoint.objects.get_or_create(code='tf', type=sp_type, 
+                                                    location=loc)
+    sp.name = 'test supply point'
+    sp.active = True
+    sp.save()
+    try:
+        hf = HealthFacility.objects.get(code='tf')
+    except HealthFacility.DoesNotExist:
+        hf_type = HealthFacilityType.objects.all()[0]
+        hf = HealthFacility.objects.create(name='test facility', 
+                                           code='tf', 
+                                           type=hf_type, 
+                                           location=loc,  
+                                           supply_point=sp)
+        if log_to_console:
+            print "  Supply point %s created" % hf.name
--- mtrack/management/commands/load_demo_data.py
+++ mtrack/management/commands/load_demo_data.py
@@ -1,59 +1,17 @@
 from django.core.management.base import BaseCommand
-from dimagi.utils.couch.database import get_db
-from django.conf import settings
-import sys
+from logistics import loader as logi_loader
+from mtrack import loader as mtrack_loader
 
 class Command(BaseCommand):
     help = "Initialize static data for ghana"
 
     def handle(self, *args, **options):
-        load_products()
-        generate_codes_for_locations()
-
-def load_products():
-    from logistics.models import Product, ProductType
-    product_types = {'m':'Malaria'
-                     }
-    products = [('yellow','Yellow ACT','m','box'), 
-                ('blue','Blue ACT','m','box'), 
-                ('brown','Brown ACT','m','box'), 
-                ('green','Green ACT','m','box'), 
-                ('other','Other ACT','m','box'), ]
-    for key in product_types.keys():
-        t, created = ProductType.objects.get_or_create(code=key, name=product_types[key])
-    for prod in products:
-        p, created = Product.objects.get_or_create(sms_code=prod[0], name=prod[1], 
-                                                   type=ProductType.objects.get(code=prod[2]), 
-                                                   units=prod[3])
-        if created:
-            print "Created product %(prod)s" % {'prod':p}
-
-def generate_codes_for_locations():
-    from rapidsms.contrib.locations.models import Location
-    locs = Location.objects.all().order_by('name')
-    for loc in locs:
-        if loc.code is None or len(loc.code) == 0:
-            loc.code = _generate_location_code(loc.name)
-            loc.save()
-            print "  %(name)s's code is %(code)s" % {'name':loc.name,
-                                                     'code':loc.code}
-
-def _generate_location_code(name):
-    from rapidsms.contrib.locations.models import Location
-    code = name.lower().replace(' ','_')
-    code = code.replace('().&,','')
-    postfix = ''
-    existing = Location.objects.filter(code=code)
-    if existing:
-        count = 1
-        postfix = str(count)
-        try:
-            while True:
-                Location.objects.get(code=(code + postfix))
-                count = count + 1
-                postfix = str(count)
-        except Location.DoesNotExist:
-            pass
-    return code + postfix
-
-    
+        logi_loader.load_products()
+        logi_loader.generate_codes_for_locations()
+        logi_loader.init_reports(True)
+        logi_loader.init_roles(True)
+        logi_loader.init_test_location_and_supplypoints()
+        logi_loader.init_test_product_and_stock()
+        mtrack_loader.init_test_facilities(True)
+        mtrack_loader.load_cvs_xforms()    
+ 
