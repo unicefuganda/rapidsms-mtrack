fcdc03cdf0879f8bcfddc554c18ffb3dad611c37
Rowena <rluk@dimagi.com>
2011-07-14 17:45:02 +0300

    small fix to xform signal to handle case where user is not associated with a facility

--- mtrack/signals.py
+++ mtrack/signals.py
@@ -27,6 +27,11 @@ def xform_received_handler(sender, **kwargs):
         submission.has_errors = True
         submission.save()
         return
+    if health_provider.facility is None:
+        submission.response = "You are not associated with a facility. Please contact an administrator."
+        submission.has_errors = True
+        submission.save()
+        return
     
     if xform.keyword == 'act':
         stock_report = ProductReportsHelper(health_provider.facility.supply_point, 
