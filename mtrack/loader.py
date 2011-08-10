94c8ef604e0fa55704b4dd54f2e45f24482ff23d
David McCann <david.a.mccann@gmail.com>
2011-08-10 17:35:59 +0300

    formatting, minor fix to pre_save handler for healthfacilities

--- mtrack/loader.py
+++ mtrack/loader.py
@@ -8,9 +8,10 @@ import sys
 from django.conf import settings
 from rapidsms.contrib.locations.models import LocationType, Location, Point
 from logistics.const import Reports
-from logistics.models import SupplyPoint, SupplyPointType,\
+from logistics.models import SupplyPoint, SupplyPointType, \
     ProductReportType, ContactRole, Product, ProductType
 from logistics.util import config
+from healthmodels.models import HealthFacility
 
 def mtrack_init():
     from logistics import loader as logi_loader
@@ -21,7 +22,7 @@ def mtrack_init():
     logi_loader.init_supply_point_types()
     logi_loader.generate_codes_for_locations()
     init_admin()
-    cvs_init_xforms()  
+    cvs_init_xforms()
     # act xform initiailization is already handled in cvs
     # mtrack_loader.init_xforms()  
     add_supply_points_to_facilities(True)
@@ -43,17 +44,17 @@ def init_test_user():
     c = Connection.objects.create(identity='8675309', backend=b)
     c.contact = hp
     c.save()
-    
+
 def init_admin():
     from django.contrib.auth.models import User
     try:
         User.objects.get(username='admin')
     except User.DoesNotExist:
-        admin = User.objects.create_user(username='admin', 
-                                 email='test@doesntmatter.com', 
+        admin = User.objects.create_user(username='admin',
+                                 email='test@doesntmatter.com',
                                  password='password')
-        admin.is_staff=True
-        admin.is_superuser=True
+        admin.is_staff = True
+        admin.is_superuser = True
         admin.save()
 
 def  _init_facility_types():
@@ -76,7 +77,7 @@ def init_test_facilities(log_to_console=False):
     try:
         sp = SupplyPoint.objects.get(code='tf')
     except SupplyPoint.DoesNotExist:
-        sp, created = SupplyPoint.objects.get_or_create(code='tf', type=sp_type, 
+        sp, created = SupplyPoint.objects.get_or_create(code='tf', type=sp_type,
                                                         location=loc)
     sp.name = 'test supply point'
     sp.active = True
@@ -85,9 +86,9 @@ def init_test_facilities(log_to_console=False):
         hf = HealthFacility.objects.get(code='tf')
     except HealthFacility.DoesNotExist:
         hf_type = HealthFacilityType.objects.all()[0]
-        hf = HealthFacility.objects.create(name='test facility', 
-                                           code='tf', 
-                                           type=hf_type,   
+        hf = HealthFacility.objects.create(name='test facility',
+                                           code='tf',
+                                           type=hf_type,
                                            supply_point=sp)
         if log_to_console:
             print "  Supply point %s created" % hf.name
@@ -95,7 +96,7 @@ def init_test_facilities(log_to_console=False):
 def init_xforms():
     from cvs.utils import init_xforms_from_tuples
     XFORMS = (
-        ('', 'act', ' ', 'ACT stock report','Report levels of ACT stocks',),
+        ('', 'act', ' ', 'ACT stock report', 'Report levels of ACT stocks',),
     )
 
     XFORM_FIELDS = {
@@ -107,7 +108,7 @@ def init_xforms():
              ('other', 'int', 'Other ACT', True),
          ],
     }
-    
+
     init_xforms_from_tuples(XFORMS, XFORM_FIELDS)
 
 def add_supply_points_to_facilities(log_to_console=False):
@@ -124,9 +125,9 @@ def add_supply_points_to_facilities(log_to_console=False):
 
 def create_supply_point_from_facility(f):
     type_, created = SupplyPointType.objects.get_or_create(code=f.type.slug)
-    sp = SupplyPoint(code=f.code, 
-                     name=f.name, 
-                     type=type_, 
+    sp = SupplyPoint(code=f.code,
+                     name=f.name,
+                     type=type_,
                      active=True)
     try:
         sp.location = get_location_from_facility(f)
@@ -141,9 +142,9 @@ def get_location_from_facility(facility):
     CAUTION: this location hierarchy is intended to match the reporting structure
     used in rapidsms-cvs. Don't modify this without ensuring consistency with those reports. 
     """
-    from django.db.models import Max,Min
-    if facility.catchment_areas.count()==0:
+    from django.db.models import Max, Min
+    if facility.catchment_areas.count() == 0:
         raise ValueError("%s does not have any catchment areas defined!" % facility.name)
-    bounds = facility.catchment_areas.aggregate(Max('rght'),Min('lft'))
-    location = Location.objects.filter(lft__lte=bounds['lft__min'],rght__gte=bounds['rght__max']).order_by('-lft')
+    bounds = facility.catchment_areas.aggregate(Max('rght'), Min('lft'))
+    location = Location.objects.filter(lft__lte=bounds['lft__min'], rght__gte=bounds['rght__max']).order_by('-lft')
     return location[0]
--- mtrack/signals.py
+++ mtrack/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import pre_save
 from rapidsms_xforms.models import xform_received
 from healthmodels.models import HealthFacility
+from healthmodels.models.HealthFacility import HealthFacilityBase
 from logistics.models import SupplyPoint, SupplyPointType
 from mtrack.loader import create_supply_point_from_facility, get_location_from_facility
 
@@ -13,7 +14,7 @@ def xform_received_handler(sender, **kwargs):
     #why am i getting errors?
     #if submission.has_errors:
     #    return
-    
+
     message = None
     if 'message' in kwargs:
         message = kwargs['message']
@@ -23,7 +24,7 @@ def xform_received_handler(sender, **kwargs):
         return
     if not message:
         return
-    
+
     try:
         health_provider = submission.connection.contact.healthproviderbase.healthprovider
     except:
@@ -31,15 +32,15 @@ def xform_received_handler(sender, **kwargs):
         submission.has_errors = True
         submission.save()
         return
-    
+
     if xform.keyword == 'act':
         if health_provider.facility is None:
             submission.response = "You are not associated with a facility. Please contact an administrator."
             submission.has_errors = True
             submission.save()
             return
-        stock_report = ProductReportsHelper(health_provider.facility.supply_point, 
-                                            const.Reports.SOH,  
+        stock_report = ProductReportsHelper(health_provider.facility.supply_point,
+                                            const.Reports.SOH,
                                             message)
         stock_report.add_product_receipt(config.Products.YELLOW_ACT, submission.eav.act_yellow_disp)
         stock_report.add_product_stock(config.Products.YELLOW_ACT, submission.eav.act_yellow_balance)
@@ -58,23 +59,28 @@ def xform_received_handler(sender, **kwargs):
 
 xform_received.connect(xform_received_handler, weak=True)
 
-def update_supply_point_from_facility(sender, instance, **kwargs): 
+def update_supply_point_from_facility(sender, instance, **kwargs):
     """ 
     whenever a facility is updated, automatically update the supply point
     """
     supply_point = instance.supply_point
+    try:
+        base = HealthFacilityBase.objects.get(pk=instance.pk)
+    except HealthFacilityBase.DoesNotExist:
+        base = instance
     if supply_point is None:
         # create
-        instance.supply_point = create_supply_point_from_facility(instance)
+        instance.supply_point = create_supply_point_from_facility(base)
         return
+
     # else update
-    type_, created = SupplyPointType.objects.get_or_create(code=instance.type.slug)
+    type_, created = SupplyPointType.objects.get_or_create(code=base.type.slug)
     supply_point.type = type_
     try:
-        supply_point.location = get_location_from_facility(instance)
+        supply_point.location = get_location_from_facility(base)
     except ValueError:
         pass
     supply_point.save()
     return
-    
+
 pre_save.connect(update_supply_point_from_facility, HealthFacility)
