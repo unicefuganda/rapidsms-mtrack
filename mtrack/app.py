d70d9e74af3318c1fb06878dc0b61046b6a585f7
miclovich <vicmiclovich@gmail.com>
2011-11-16 15:13:12 +0300

    quick changes

--- mtrack/app.py
+++ mtrack/app.py
@@ -1,29 +1,20 @@
-import rapidsms, datetime, re
+import rapidsms, datetime
 from rapidsms.apps.base import AppBase
 from contact.models import Flag, MessageFlag
 from poll.models import Poll
 from script.models import Script, ScriptProgress
 from rapidsms.models import Contact
+from mtrack.models import AnonymousReport
 
 class App(AppBase):
     def handle(self, message):
-        if not message.connection.contact and not ScriptProgress.objects.filter(script__slug="mtrac_anonymous_autoreg",connection=message.connection).exists():
-            ScriptProgress.objects.create(script=Script.objects.get(slug="mtrac_anonymous_autoreg"),connection=message.connection)
+        if message.connection.backend.name == 'yo8200':
+            d = datetime.datetime.now() - datetime.timedelta(hours=1)
+            if not AnonymousReport.objects.filter(date__gte=d, connection=message.connection).exists():
+                ScriptProgress.objects.create(script=Script.objects.get(slug="mtrack_anonymous_autoreg"), connection=message.connection)
+                ar = AnonymousReport.objects.create(connection=message.connection)
+            elif ScriptProgress.objects.filter(script__slug="mtrac_anonymous_autoreg", connection=message.connection).exists():
+                ar = AnonymousReport.objects.filter(connection=message.connection).latest('date')
+                return False
+            ar.messages.add(message.db_message)
             return True
-        flags = Flag.objects.values_list('name',flat=True).distinct()
-        one_template = r"(.*\b(%s)\b.*)"
-        w_regex = []
-        for word in flags:
-            w_regex.append(one_template % str(word).strip())
-        reg = re.compile(r"|".join(w_regex))
-        match = reg.search(message.text)
-        print match # test what's been matched
-        if match:
-            if hasattr(message,"db_message"):
-                db_message = message.db_message
-                try:
-                    flag = Flag.objects.get(name=[d for d in list(match.groups()) if d][1])
-                except (Flag.DoesNotExist,IndexError):
-                    flag = None
-                MessageFlag.objects.create(message=db_message,flag=flag)
-        return False
