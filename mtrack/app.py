4a8d6c6ee2aaf841763e18a158b0d30d59f98cf9
miclovich <vicmiclovich@gmail.com>
2011-12-12 16:25:50 +0300

    Fixes

--- mtrack/app.py
+++ mtrack/app.py
@@ -1,24 +1,24 @@
-import rapidsms, datetime
-from django.conf import settings
-from rapidsms.apps.base import AppBase
 from contact.models import Flag, MessageFlag
-from poll.models import Poll
-from script.models import Script, ScriptProgress
-from rapidsms.models import Contact
+from django.conf import settings
 from mtrack.models import AnonymousReport
-#sending messages??? outbound!
+from poll.models import Poll
+from rapidsms.apps.base import AppBase
+from rapidsms.models import Contact
 from rapidsms_httprouter.models import Message
+from script.models import Script, ScriptProgress
+import rapidsms
+import datetime
+#sending messages??? outbound!
 
 class App(AppBase):
     def handle(self, message):
         if message.connection.backend.name == getattr(settings, 'HOTLINE_BACKEND', 'console'):            
             d = datetime.datetime.now() - datetime.timedelta(hours=1)
             #snatch and compare every immediate SMS connections & timestamps to existing Anonymous Reports messages
-            ar = AnonymousReport.objects.create(connection=message.connection, message=message.db_message)
+            AnonymousReport.objects.create(connection=message.connection, message=message.db_message)                        
             Message.objects.create(direction="O",
-                                   text=u"Thank you for your report! Webaale kututegeezako!",
+                                   text="Thank you for your report! Webaale kututegeezako!",
                                    status='Q',
                                    connection=message.connection,
-                                   in_response_to=ar.message)
-            ar.save()
+                                   in_response_to=message)            
             return True
