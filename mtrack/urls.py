7854494fa74e071a6a9fa538dfb2efcc9ef47b18
miclovich <vicmiclovich@gmail.com>
2011-12-16 19:20:47 +0300

    actions based sorting

--- mtrack/urls.py
+++ mtrack/urls.py
@@ -35,7 +35,9 @@ urlpatterns = patterns('',
     # login required added
     url(r'^anonymousreports/$', login_required(generic), {
         'model':AnonymousReport,  
-        'queryset': AnonymousReport.objects.all().order_by('-date','action'), #action --> analogous to status of report      
+        # primitive filtering by actions
+        #TODO subclass SimpleSorter to sort actions
+        'queryset': AnonymousReport.objects.all().order_by('action','-date'), #action --> analogous to status of report      
         'objects_per_page':25,
         'base_template':'mtrack/mtrack_generic_base.html',
         'partial_row':'mtrack/partials/anon_row.html',
