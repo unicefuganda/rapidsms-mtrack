68cafa30152cd58449f02585588f7760588cf572
miclovich <vicmiclovich@gmail.com>
2011-12-16 15:12:12 +0300

    util script to support stable prod environment

--- mtrack/testbatch.py
+++ mtrack/testbatch.py
@@ -1,4 +1,6 @@
-from .models import AnonymousReportBatch, AnonymousReport
+# quick util script for batching up anonymous reports (not used anywhere else in the production)
+from .models import AnonymousReport
+from rapidsms_httprouter.models import Message
 import datetime
 
 hour_range = [i+1 for i in range(10*24)]
@@ -6,12 +8,9 @@ end_epoch = datetime.datetime.now()
 get_all = []
 for hour in hour_range:
     try:
-        ars = AnonymousReport.objects.filter(date__range=[end_epoch-datetime.timedelta(hours=hour), end_epoch])
+        # go through all messages (inbound through Yo8200
+        messagi = Message.objects.filter(connection__backend__name="yo8200", direction="I",
+            date__range=[end_epoch-datetime.timedelta(hours=hour),end_epoch])
         get_all.append(ars)
     except IndexError:
         pass
-
-if AnonymousReportBatch.objects.filter(date__range=[start_epoch, end_epoch], connection__in=Connection.objects.filter(id=message.connection.id)).exists():
-
-all_areports = AnonymousReport.objects.all()
-d = datetime.datetime.now() - datetime.timedelta(hours=1)
