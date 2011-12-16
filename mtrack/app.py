ff35ad10ffec45af8f9ba58d5bc419403b3c7ca4
miclovich <vicmiclovich@gmail.com>
2011-12-16 18:40:47 +0300

    time fixes

--- mtrack/app.py
+++ mtrack/app.py
@@ -13,10 +13,10 @@ class App(AppBase):
             # if anonymous report gets in and its time stamp is within the limit of 1hr
             # add this report to an existing AnonymousReportBatch object
             end_epoch = datetime.datetime.now()
-            start_epoch = end_epoch - datetime.timedelta(hours=1)
-            if AnonymousReport.objects.filter(date__range=[start_epoch, end_epoch], connection__in=Connection.objects.filter(id=message.connection.id)).exists():
+            epoch = end_epoch - datetime.timedelta(seconds=3600)            
+            if AnonymousReport.objects.filter(date__gte=epoch, connection__in=Connection.objects.filter(id=message.connection.id)).exists():
                 try:
-                    anonymous_report = AnonymousReport.objects.filter(date__range=[start_epoch,end_epoch], connection__in=Connection.objects.filter(id=message.connection.id))[0]
+                    anonymous_report = AnonymousReport.objects.filter(date__gte=epoch, connection__in=Connection.objects.filter(id=message.connection.id))[0]
                     anonymous_report.messages.add(message.db_message)
                     anonymous_report.save()
                     Message.objects.create(direction="O",
