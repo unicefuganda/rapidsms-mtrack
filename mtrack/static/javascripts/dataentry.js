272bdf6101496796da1ea1dee56a24aa8dc4bac8
Samuel Sekiwere <sekiskylink@gmail.com>
2012-07-17 22:29:05 +0300

    Input HMIS 033B from dashboard

--- mtrack/static/javascripts/dataentry.js
+++ mtrack/static/javascripts/dataentry.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-    /*Look at django-selectable*/
+    /*Build array with required fields*/
+    var mandatory_fields = ['muac_red', 'death', 'fastbreath', 'diarrhea',
+                            'muac_green', 'fever', 'muac_yellow', 'bi_od'
+        ]
     /*When district is changed*/
+    $('#rdate').datepicker({dateFormat: 'yy-mm-dd'});
+    $('#fsubmit').hide();
     $('#district').change(function(){
         var districtid = $(this).val();
-        if (districtid == '0')
+        if (districtid == '0' || districtid == "")
             return;
         // $('#facility').find('option').remove().end();
         $('#facility').empty();
@@ -31,10 +36,10 @@ $(document).ready(function(){
     /*When facility is changed*/
     $('#facility').change(function(){
         var facilityid = $(this).val();
-        if (facilityid == '0')
+        if (facilityid == '0' || facilityid == '')
             return;
         $('#reporter').find('option').remove().end();
-        $('#reporter').append("<option value='0' selected='selected'>Select Reporter</option>");
+        $('#reporter').append("<option value='' selected='selected'>Select Reporter</option>");
         $.get(
             '/ajax_portal/',
             {xtype:'facility', xid: facilityid},
@@ -52,6 +57,29 @@ $(document).ready(function(){
         );
     });
 
+    /*When Report Type is changed*/
+    $('#rtype').change(function(){
+        var typeid = $(this).val();
+        if (typeid == '0' || typeid == '')
+            return;
+
+        $('#xform').find('option').remove().end();
+        $('#xform').append("<option value='0' selected='selected'>Select Report</option>");
+        $.get('/ajax_portal/',
+            {'xtype':'report',xid:typeid},
+            function(data){
+                var xforms = data;
+                for (var i in xforms) {
+                    val = xforms[i]['id'];
+                    txt = xforms[i]['name'] + "("+xforms[i]['keyword'] + ")";
+                    $('#xform').append($(document.createElement("option")).attr("value",val).text(txt))
+                }
+            },
+            'json'
+        );
+
+    });
+
     /*Some initial stuff*/
     // $('#ffields').hide();
 
@@ -59,6 +87,8 @@ $(document).ready(function(){
     $('#xform').change(function(){
         var xformid = $(this).val();
         if (xformid == '0'){
+            $('#fsubmit').hide();
+            $('#xff_table').empty();
             return;
         }
         $.get('/ajax_portal/',
@@ -74,16 +104,28 @@ $(document).ready(function(){
                     field_type = xformfields[i]['field_type'];
                     field_id = 'id_'+command;
                     if (field_type == 'binary'){
-                        htmlstr += "<tr><td>" + name +"</td><td><input type='file' name='"+ command +"' id='" + field_id + "' class='itext'/></td></tr>";
+                        label_str = "<label for='" + field_id +"'>"+name.charAt(0).toUpperCase() +name.slice(1) + ":</label>";
+                        htmlstr += "<tr><td>" + label_str +"</td><td><input type='file' name='"+
+                        command +"' id='" + field_id + "' class='itext" + validation_class +"'/></td></tr>";
                     }else{
-                        htmlstr += "<tr><td>" + name +"</td><td><input type='text' name='"+ command +"' id='" + field_id + "' class='itext'/></td></tr>";
+                        if ($.inArray(command,mandatory_fields) > -1){
+                            validation_class = ' required'
+                        } else {
+                            validation_class = ''
+                        }
+                        label_str = "<label for='" + field_id + "'>"+name.charAt(0).toUpperCase() +name.slice(1) + ":</label>";
+                        htmlstr += "<tr><td>" + label_str +"</td><td><input type='text' name='"+
+                        command +"' id='" + field_id + "' class='itext" + validation_class + "'/></td></tr>";
                     }
                 }
                 $('#xff_table').append(htmlstr);
                 /* Add Submit buttons*/
+                $('#fsubmit').show();
             },
             'json'
         );
     });
     /*More stuff*/
+    /*Validation*/
+    $('#dataForm').validate({messages:{reporter: "Reporter is required", rdate:"date is required"}});
 });
--- mtrack/views/dataentry.py
+++ mtrack/views/dataentry.py
@@ -5,29 +5,49 @@ from healthmodels.models.HealthFacility import HealthFacility
 from healthmodels.models.HealthProvider import HealthProvider
 from rapidsms_xforms.models import XForm, XFormSubmission
 from rapidsms_xforms.views import make_submission_form
+from rapidsms_httprouter.models import Message
 from django.http import HttpResponse
 from django.utils import simplejson
+import datetime
 
 def data_entry(request):
     #consider a list
     districts = Location.objects.filter(type__name='district').values('id', 'name').order_by('name')
-    facilities = HealthFacility.objects.all().values('id', 'name', 'type__slug').order_by('name')
+    #facilities = HealthFacility.objects.all().values('id', 'name', 'type__slug').order_by('name')
+    facilities = [(0, 'Select Facility')]
     reporters = [(0, 'Select Reporter')]
-    xforms = XForm.objects.all().values('id', 'name', 'keyword').order_by('name')
+    xforms = []
+    #xforms = XForm.objects.all().values('id', 'name', 'keyword').order_by('name')
     if request.method == 'POST':
-        print request.POST
         xform = request.POST['xform']
         reporterid = request.POST['reporter']
         reporter = HealthProvider.objects.get(pk=reporterid)
         the_xform = XForm.on_site.get(pk=xform)
+        rdate = request.POST['rdate']
         form_class = make_submission_form(the_xform)
 
         form = form_class(request.POST, request.FILES)
         if form.is_valid():
+            msg = the_xform.keyword.upper()
+            for k,v in form.cleaned_data.iteritems():
+                if k not in ['districts','reporter', 'facility', 'rdate', 'rtype']:
+                    if v or v in ('0',0):
+                        msg +="." + "%s."%k + "%s"%v
+            #create message here and insert into database!!
+            cdate = datetime.datetime.strptime(rdate, '%Y-%m-%d')
+            MsgObj = None
+            if len(msg) > len(the_xform.keyword):
+                MsgObj = Message.objects.create(text=msg, direction='I', status='H',
+                        connection=reporter.default_connection)
+                MsgObj.date = cdate
+                MsgObj.save()
             #now lets create a submission for this xform
             submission = XFormSubmission.objects.create(xform=the_xform, connection=reporter.default_connection)
             the_xform.update_submission_from_dict(submission, form.cleaned_data)
             #remember to set the reporter
+            if MsgObj:
+                submission.message = MsgObj
+                submission.created = cdate
             submission.save()
     else:
         pass
@@ -48,11 +68,15 @@ def ajax_portal(request):
         response = facilities
     elif xtype == 'facility':
         response = list(HealthProvider.objects.exclude(connection=None).filter(facility=xid)\
+                        .exclude(groups__name__in=['FHD'])\
                         .values('id', 'name', 'connection__identity').order_by('name'))
     elif xtype == 'xform':
         xform = XForm.on_site.get(pk=xid)
         fields = xform.fields.all().order_by('order').values('name', 'command', 'field_type')
         response = list(fields)
+    elif xtype == 'report':
+        qq = xid.replace('[', '').replace(']', '').split(',')
+        response = list(XForm.objects.filter(keyword__in=qq).values('id', 'name', 'keyword').order_by('name'))
     else:
         response = []
     json = simplejson.dumps(response)
