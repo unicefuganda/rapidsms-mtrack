c6b86d4d5b6b25b8df81a8048053af8363bbd212
Kenneth Matovu <matovken@gmail.com>
2013-02-06 12:10:23 +0300

    logging

--- mtrack/utils.py
+++ mtrack/utils.py
@@ -132,10 +132,10 @@ def get_facility_reports(location, count=False, date_range=last_reporting_period
     k = HealthFacility.objects.filter(name__icontains='Budongo')[0]
     logger.info("%s: %s"%(k, k in facilities))
     s = k.xformsubmissionextras_set.latest('cdate').submission
-    logger.info("%s - has_errors = %s, created = %s, Xform in = %s: "%(k,s.has_errors, s.created, s.xform.keyword in ['act', 'cases', 'death', 'opd', 'test', 'treat', 'rdt', 'qun']))
+    logger.info("%s - has_errors = %s, created = %s, Xform in = %s:,approved = %s "%(k,s.has_errors, s.created, s.xform.keyword in ['act', 'cases', 'death', 'opd', 'test', 'treat', 'rdt', 'qun'],s.approved))
     toret = XFormSubmission.objects.filter(connection__contact__healthproviderbase__facility__in=facilities, has_errors=False,
         xform__keyword__in=['act', 'cases', 'death', 'opd', 'test', 'treat', 'rdt', 'qun']).order_by('-created')
-
+    logger.info('%s: %s'%k, s in toret)
     if date_range:
         toret = toret.filter(created__range=date_range)
     if approved is not None:
