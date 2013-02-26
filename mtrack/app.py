daf4cefae9c1e0e636ac0ad7b937c5a53a212ff3
Samuel Sekiwere <sekiskylink@gmail.com>
2013-02-26 06:51:04 +0300

    support dhis2 keywords, and call URL for sms input

--- mtrack/app.py
+++ mtrack/app.py
@@ -5,18 +5,31 @@ from rapidsms.models import Connection
 from rapidsms.apps.base import AppBase
 from rapidsms_httprouter.models import Message
 from uganda_common.utils import handle_dongle_sms
+from urllib import urlopen, urlencode
 
 class App(AppBase):
     def handle(self, message):
         if handle_dongle_sms(message):
                     return True
+        if message.text.lower().startswith('mcd') or message.text.lower().strip().startswith('pmtct'):
+            url = getattr(settings, 'DHIS2_SMSINPUT_URL', 'http://localhost:9090/dhis2_smsinput?')
+            if url.find('?') < 0:
+                c = '?'
+            else:
+                c = ''
+            url = url + c + 'message=%s&sender=%s' % (message.text, message.connection.identity)
+            try:
+                urlopen(url)
+            except:
+                pass
+            return True
         if message.connection.backend.name == getattr(settings, 'HOTLINE_BACKEND', 'console'):
-            #anonymous_report = AnonymousReport.objects.create(connection=message.connection, message=message.db_message)
-            #anonymous_report.save()
+            # anonymous_report = AnonymousReport.objects.create(connection=message.connection, message=message.db_message)
+            # anonymous_report.save()
             # if anonymous report gets in and its time stamp is within the limit of 1hr
             # add this report to an existing AnonymousReportBatch object
             end_epoch = datetime.datetime.now()
-            epoch = end_epoch - datetime.timedelta(seconds=3600*24*3)
+            epoch = end_epoch - datetime.timedelta(seconds=3600 * 24 * 3)
             if AnonymousReport.objects.filter(date__gte=epoch, connection__in=Connection.objects.filter(id=message.connection.id)).exists():
                 try:
                     anonymous_report = AnonymousReport.objects.filter(date__gte=epoch, connection__in=Connection.objects.filter(id=message.connection.id))[0]
