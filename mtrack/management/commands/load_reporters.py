09ae90600abaeb971bed2299ee0ccc6e8571d38a
Samuel Sekiwere <sekiskylink@gmail.com>
2013-08-13 10:59:29 +0300

    improve reporter loading/registration script

--- mtrack/management/commands/load_reporters.py
+++ mtrack/management/commands/load_reporters.py
@@ -39,83 +39,111 @@ class Command(BaseCommand):
             for rownum in range(sh.nrows):
                 vals = sh.row_values(rownum)
                 l.append(vals)
-        print l
+        #print l
         return l
 
-    @transaction.commit_manually
+    @transaction.commit_on_success
     def load_reporters(self, data):
-        n = 0
-        transaction.commit()
+        IGNORED_ENTRIES = []
         for d in data:
-            n+=1
-            print 'n = ',n
-            print 'back up'
             if not d[self.order['name']] or not d[self.order['phone']]:
                 print "no name"
+                IGNORED_ENTRIES.append(d)
                 continue
             print d
-            _name = d[self.order['name']].strip().lower().capitalize()
-            _phone = '%s' % d[self.order['phone']]
-            print "=====================>", _phone
-            _district = d[self.order['district']].strip()
+            _name = d[self.order['name']].strip()
+            _name = ' '.join([pname.capitalize() for pname in _name.split(' ')])
+            _phone = '%s' % (str(int(d[self.order['phone']])) if '/' not in '%s' % \
+                    d[self.order['phone']]  else d[self.order['phone']])
+            #print "=====================>", _phone
+            _district = d[self.order['district']].strip().capitalize()
             _role = d[self.order['role']].strip()
             _fac = d[self.order['facility']].strip()
             _fac_type = d[self.order['facility_type']].replace(' ','').lower()
-            print _fac_type
-            _village = d[self.order['village']].strip()
-            _village_type = d[self.order['village_type']].strip()
+            _village = d[self.order['village']].strip().capitalize()
+            _village_type = d[self.order['village_type']].strip().lower()
 #            _pvht = d[self.order['pvht']]
-            _phone = _phone.strip().replace('.','').split('e')[0].split('.')[0].replace('-', '')
+            #_phone = _phone.strip().replace('.','').split('e')[0].split('.')[0].replace('-', '')
             nums = _phone.split('/')
             _phone2 = ''
 #            _village_name = d[self.order['village_name']].capitalize()
             already_exists = False
-            print nums
+            #print nums
             if len(nums) > 1:
                 _phone = nums[0]
                 _phone2 = nums[1]
 
-            print _name, _phone, _role
-            district = find_closest_match(_district, Location.objects.filter(type='district'))
+            #print _name, _phone, _role
+            district = find_closest_match(_district, Location.objects.filter(\
+                    type='district'), match_exact=True)
             #roles = list(Group.objects.filter(name__in=_role.split(',')))
-            print district
             if not district:
-                transaction.commit()
+                print "WARNING: District Not Found (%s)" % _district
+                IGNORED_ENTRIES.append(d)
                 continue
-            #try:
-            #    facility = find_closest_match(_fac, HealthFacility.objects.filter(type__name__in=[_fac_type]).\
-            #            filter(catchment_areas__in=district.get_descendants(include_self=True)))
-            #except HealthFacility.MultipleObjectsReturned:
-            #    xx = HealthFacility.objects.filter(name=_fac,type__name=_fac_type).\
-            #            filter(catchment_areas__in=district.get_descendants(include_self=True))
-            #    facility = xx[0] if xx else ''
+                print "============================================================================="
             facility = ''
+
             if not facility:
-                xx = HealthFacility.objects.filter(name__iexact="%s" % _fac, type__name=_fac_type).\
+                xx = HealthFacility.objects.filter(name__iexact="%s" % _fac, type__slug=_fac_type).\
                         filter(catchment_areas__in=district.get_descendants(include_self=True))
                 facility = xx[0] if xx else ''
-                print 'facility --1',xx.count()
+                #print 'facility count  on 1st search',xx.count()
                 if not facility:
-                    facility = HealthFacility.objects.filter(name__iexact=_fac).filter(catchment_areas__in=district.get_descendants(include_self=True))
-                    print 'facility --2',facility.count()
+                    facility = HealthFacility.objects.filter(name__iexact=_fac).filter(\
+                            catchment_areas__in=district.get_descendants(include_self=True))
+                    #print 'facility count on 2nd search',facility.count()
                     if facility.exists(): facility = facility[0]
                 if not facility:
-                    transaction.commit()
+                    try:
+                        facility = find_closest_match(_fac, HealthFacility.objects.filter(\
+                                type__slug__in=[_fac_type]).filter(catchment_areas__in=\
+                                district.get_descendants(include_self=True)), match_exact=True)
+                    except HealthFacility.MultipleObjectsReturned:
+                        xx = HealthFacility.objects.filter(name=_fac,type__slug=_fac_type).\
+                                filter(catchment_areas__in=district.get_descendants(include_self=True))
+                        facility = xx[0] if xx else ''
+                if not facility:
                     print "WARNING: Facility Not Found (%s %s)" % (_fac, _fac_type)
+                    IGNORED_ENTRIES.append(d)
+                    print "============================================================================="
                     continue
+            print "INFO: Facility Found: (%s)" % facility.name
             village = None
             if _village:
                 if district:
-                    village = find_closest_match(_village, Location.objects.filter(type__in=[_village_type]))
-                    print 'at village'
-                    transaction.commit()
+                    #village = find_closest_match(_village, Location.objects.filter(type__in=[_village_type]))
+                    if facility.catchment_areas.all():
+                        village = facility.catchment_areas.all()[0]
+                    else:
+                        village = find_closest_match(_village, district.get_descendants(include_self=True).\
+                                filter(type__in=[_village_type]), match_exact=True)
+                    #print 'at village'
+            else:
+                    if facility.catchment_areas.all():
+                        village = facility.catchment_areas.all()[0]
+
 #            if _name:
 #                _name = ' '.join([n.capitalize() for n in _name.lower().split()])
-            msisdn, backend = assign_backend(_phone)
-            print msisdn, village
+            if len(_phone) > 12:
+                print "WARINIG: Invalid Number (%s) -skipped" % _phone
+                IGNORED_ENTRIES.append(d)
+                print "============================================================================="
+                continue
+            try:
+                msisdn, backend = assign_backend(_phone)
+            except:
+                print "WARINIG: Invalid Number (%s) -skipped" % _phone
+                IGNORED_ENTRIES.append(d)
+                print "============================================================================="
+                continue
+            #print msisdn, village
 
             if _phone2:
-                msisdn2, backend2 = assign_backend(_phone2)
+                try:
+                    msisdn2, backend2 = assign_backend(_phone2)
+                except:
+                    print "WARNING: PhoneNumber Ignored (%s)" % _phone2
 
             connection2 = None
             try:
@@ -142,7 +170,7 @@ class Command(BaseCommand):
                 already_exists = True
             except Contact.DoesNotExist, Contact.MultipleObjectsReturned:
                 contact = HealthProvider.objects.create()
-                transaction.commit()
+                #transaction.commit()
 
             connection.contact = contact
             connection.save()
@@ -151,7 +179,6 @@ class Command(BaseCommand):
 
             if facility:
                 contact.facility = facility
-                transaction.commit()
             if village:
                 contact.reporting_location = village
                 contact.village = village
@@ -163,7 +190,6 @@ class Command(BaseCommand):
                     contact.reporting_location = Location.tree.root_nodes()[0]
                 contact.village_name = _village
                 contact.village = None
-            transaction.commit()
             current_groups = list(contact.groups.all().values_list('name', flat=True))
             new_roles = list(set(_role.split(',') + current_groups))
             roles = list(Group.objects.filter(name__in=new_roles))
@@ -175,5 +201,19 @@ class Command(BaseCommand):
             if connection2:
                 contact.connection_set.add(connection2)
             contact.save()
-            transaction.commit()
-            print contact.id
+            print "INFO: HealthProvider Created[contact_id:%s, identity:(%s,%s)]" % (contact.id, \
+                    '%s' % connection.identity if connection else '', connection2.identity if connection2 else '')
+            print "============================================================================="
+        print "**********************************************************************************************"
+        print "*\t\t\t", "IGNORED ENTRIES FOLLOW"
+        print "**********************************************************************************************"
+        if IGNORED_ENTRIES:
+            import codecs
+            reject_filename = "/tmp/rejects.csv"
+            file_hadle = codecs.open(reject_filename, 'w+', "utf-8")
+            for item in IGNORED_ENTRIES:
+                val = ','.join(['%s' % str(int(i)) if hasattr(i, '__int__') else i for i in item])
+                print val
+                file_hadle.write(val+"\n")
+            file_hadle.close()
+
